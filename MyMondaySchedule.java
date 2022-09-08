1e759c3575258c5fc3a799464c9518eedff4c2d6
Jranae <112411872+Jranae@users.noreply.github.com>
2022-09-08 14:51:37 -0400

    Add files via upload

---
+++ MyMondaySchedule.java
@@ -0,0 +1,16 @@
+public class MyMondaySchedule
+{
+public static void main(String[] args)
+{
+System.out.println("On Monday I wake up at " + 6 + "AM");
+System.out.println("On Monday I go to school at " + 10 + "AM");
+System.out.println("On Monday my class class is at " + 6 + "PM");
+System.out.println("On Monday I eat lunch at " + 1 + "PM");
+System.out.println("On Monday I have work study at " + 3 + "PM");
+System.out.println("On Monday I eat dinner at " + 7 + "PM");
+System.out.println("On Monday I do my homework at " + 7 + "PM");
+System.out.println("On Monday I take a nap at " + 2 + "PM");
+System.out.println("On Monday I watch tv at " + 8 + "PM");
+System.out.println("On Monday I go to sleep at " + 11 + "PM");
+}
+}
